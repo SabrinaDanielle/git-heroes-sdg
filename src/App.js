4331e2790f9b22a03d6f8dae667e03329d737432
brinadanielle <bri.daniG@gmail.com>
2017-09-03 18:10:37 -0500

    react assignment part 1, prior to routing/refractoring

--- src/App.js
+++ src/App.js
@@ -18,14 +18,87 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      title = "Tour of GIT Heroes",
+      title : "Tour of GIT Heroes",
       heroes: HEROES,
-
-    }
+      selectedHero: {
+        name:"",
+        id: undefined
+      }
+    };
+    this.handleSelectHero = this.handleSelectHero.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
   }
-  render() {
+
+  handleSelectHero(hero){
+   const heroIndex = this.state.heroes.map(o => o.id).indexOf(hero.id);
+   this.setState({
+     selectedHero:{
+       ...hero,
+       index: heroIndex
+   }
+   });
+  }
+
+  handleOnChange(event){
+    this.setState({
+      selectedHero: {
+        ...this.state.selectedHero,
+        name: event.target.value
+      }
+    })
+  }
+
+  handleOnSubmit(event) {
+    this.setState({
+        heroes: [
+          ...this.state.heroes.slice(0, this.state.selectedHero.index),
+          {...this.state.selectedHero},
+        ...this.state.heroes.slice(
+           this.state.selectedHero.index + 1,
+           this.state.heroes.length  )
+
+        ],
+      
+    });
+    event.preventDefault();
+  }
+
+  render(){
+    const heroList = this.state.heroes.map(function
+    (hero) {
     return (
-     <div>HELLO</div>
+      <li
+      className="" 
+      key={hero.id} 
+      onClick={() => this.handleSelectHero(hero)}>
+      <span className="badge">{hero.id}
+      </span> {hero.name}
+      </li>
+    );
+  }, this);
+
+    return (
+     <div>
+       <h1>{this.state.title}</h1>
+       <ul className="heroes">{heroList}</ul>
+       <div>
+        <h2>{this.state.selectedHero.name}</h2>
+         <div>
+           <label>id: </label>
+           {this.state.selectedHero.id}
+         </div>
+         <form onSubmit={this.handleOnSubmit}>
+         <label>name: </label>
+         <input type="text" value=
+         {this.state.selectedHero.name}
+         onChange={this.handleOnChange}
+         />
+         <input className="button" type="submit"
+          value="submit" />
+         </form>
+       </div>
+     </div>
     );
   }
 }
